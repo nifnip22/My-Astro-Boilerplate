1392cc58e5bd6f3ffb6915ab948e652301eb9413
nifnip22 <proxigaming742@gmail.com>
2024-10-22 19:02:21 +0800

    Modified register & email verification API

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -4,55 +4,64 @@ import { db, userTable } from '../../../lib/db';
 import { eq } from 'drizzle-orm';
 
 export async function POST(context: APIContext): Promise<Response> {
-	const formData = await context.request.formData();
+	try {
+		const formData = await context.request.formData();
 
-	const email = formData.get('email');
-	if (typeof email !== 'string' || !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
-		return new Response(JSON.stringify({ error: 'Invalid email' }), {
-			status: 400,
+		const email = formData.get('email');
+		if (typeof email !== 'string' || !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
+			return new Response(JSON.stringify({ error: 'Invalid email' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const password = formData.get('password');
+		if (typeof password !== 'string' || password.length < 8 || password.length > 20) {
+			return new Response(JSON.stringify({ error: 'Invalid password' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const existingUser = await db.select().from(userTable).where(eq(userTable.email, email.toLowerCase())).execute();
+		if (!existingUser || existingUser.length === 0) {
+			return new Response(JSON.stringify({ error: 'Incorrect username or password' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const userId = existingUser[0].id;
+		const passwordHash = existingUser[0].password;
+
+		const validPassword = await Bun.password.verify(password, passwordHash);
+
+		if (!validPassword) {
+			return new Response(JSON.stringify({ error: 'Incorrect username or password' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const session = await lucia.createSession(userId, {});
+		const sessionCookie = lucia.createSessionCookie(session.id);
+		context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+
+		return context.redirect('/dashboard');
+	} catch (error) {
+		return new Response(JSON.stringify({ error: 'Login process failed, please try again' }), {
+			status: 500,
 			headers: {
 				'Content-Type': 'application/json',
-			},
-		});
+			}
+		})
 	}
-
-	const password = formData.get('password');
-	if (typeof password !== 'string' || password.length < 6 || password.length > 255) {
-		return new Response(JSON.stringify({ error: 'Invalid password' }), {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	}
-
-	const existingUser = await db.select().from(userTable).where(eq(userTable.email, email.toLowerCase())).execute();
-	if (!existingUser || existingUser.length === 0) {
-		return new Response(JSON.stringify({ error: 'Incorrect username or password' }), {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	}
-
-	const userId = existingUser[0].id;
-	const passwordHash = existingUser[0].password;
-
-	const validPassword = await Bun.password.verify(password, passwordHash);
-
-	if (!validPassword) {
-		return new Response(JSON.stringify({ error: 'Incorrect username or password' }), {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	}
-
-	const session = await lucia.createSession(userId, {});
-	const sessionCookie = lucia.createSessionCookie(session.id);
-	context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-
-	return context.redirect('/dashboard');
 }
--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -6,91 +6,108 @@ import { eq } from 'drizzle-orm';
 import nodemailer from 'nodemailer';
 
 export async function POST(context: APIContext): Promise<Response> {
-	const formData = await context.request.formData();
+	try {
+		const formData = await context.request.formData();
 
-	const username = formData.get('username');
-	// username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
-	// keep in mind some database (e.g. mysql) are case insensitive
-	if (typeof username !== 'string' || username.length < 3 || username.length > 31) {
-		return new Response(JSON.stringify({ error: 'Invalid username, must be between 4 ~ 31 characters' }), {
-			status: 400,
+		const username = formData.get('username');
+		if (typeof username !== 'string' || username.length < 3 || username.length > 30) {
+			return new Response(JSON.stringify({ error: 'Invalid username, must be between 3 ~ 30 characters' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const email = formData.get('email');
+		if (typeof email !== 'string' || !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
+			return new Response(JSON.stringify({ error: 'Invalid email, must be a valid email' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const existingUser = await db.select().from(userTable).where(eq(userTable.email, email)).limit(1).execute();
+		if (existingUser.length > 0) {
+			return new Response(JSON.stringify({ error: 'Email already in use' }), {
+				status: 409,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const password = formData.get('password');
+		if (typeof password !== 'string' || password.length < 8 || password.length > 20) {
+			return new Response(JSON.stringify({ error: 'Invalid password, must be between 8 ~ 20 characters' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const confirmPassword = formData.get('confirm_password');
+		if (password !== confirmPassword) {
+			return new Response(JSON.stringify({ error: 'Password and confirm password do not match' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const userId = generateIdFromEntropySize(10); // 16 characters long
+
+		const passwordHash = await Bun.password.hash(password, {
+			// recommended minimum parameters
+			algorithm: 'argon2id', // "argon2id" | "argon2i" | "argon2d"
+			memoryCost: 4, // memory usage in kibibytes
+			timeCost: 3,
+		});
+
+		await db.insert(userTable).values({
+			id: userId,
+			username: username,
+			email: email,
+			emailVerified: false,
+			password: passwordHash,
+			createdAt: new Date(),
+		});
+
+		const verificationCode = await generateEmailVerificationCode(userId, email);
+
+		try {
+			await sendVerificationCode(email, verificationCode);
+		} catch (error) {
+			return new Response(JSON.stringify({ error: 'Email verification failed' }), {
+				status: 500,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		const session = await lucia.createSession(userId, {});
+		const sessionCookie = lucia.createSessionCookie(session.id);
+
+		return new Response(JSON.stringify({ success: 'Email verification in process, please wait' }), {
+			status: 200,
+			headers: {
+				'Content-Type': 'application/json',
+				'Set-Cookie': sessionCookie.serialize(),
+			},
+		});
+	} catch (error) {
+		return new Response(JSON.stringify({ error: 'An unexpected error occurred' }), {
+			status: 500,
 			headers: {
 				'Content-Type': 'application/json',
 			},
 		});
 	}
-
-	const email = formData.get('email');
-	if (typeof email !== 'string' || !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
-		return new Response(JSON.stringify({ error: 'Invalid email, must be a valid email' }), {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	}
-
-	const existingUser = await db.select().from(userTable).where(eq(userTable.email, email)).limit(1).execute();
-	if (existingUser.length > 0) {
-		return new Response(JSON.stringify({ error: 'Email already in use' }), {
-			status: 409,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	}
-
-	const password = formData.get('password');
-	if (typeof password !== 'string' || password.length < 6 || password.length > 255) {
-		return new Response(JSON.stringify({ error: 'Invalid password, must be between 6 ~ 255 characters' }), {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	}
-
-	const confirmPassword = formData.get('confirm_password');
-	if (password !== confirmPassword) {
-		return new Response(JSON.stringify({ error: 'Password and confirm password do not match' }), {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	}
-
-	const userId = generateIdFromEntropySize(10); // 16 characters long
-
-	const passwordHash = await Bun.password.hash(password, {
-		// recommended minimum parameters
-		algorithm: 'argon2id', // "argon2id" | "argon2i" | "argon2d"
-		memoryCost: 4, // memory usage in kibibytes
-		timeCost: 3,
-	});
-
-	await db.insert(userTable).values({
-		id: userId,
-		username: username,
-		email: email,
-		emailVerified: false,
-		password: passwordHash,
-		createdAt: new Date(),
-	});
-
-	const verificationCode = await generateEmailVerificationCode(userId, email);
-	await sendVerificationCode(email, verificationCode);
-
-	const session = await lucia.createSession(userId, {});
-	const sessionCookie = lucia.createSessionCookie(session.id);
-
-	return new Response(JSON.stringify({ success: 'Email verification in process, please wait' }), {
-		status: 200,
-		headers: {
-			'Content-Type': 'application/json',
-			'Set-Cookie': sessionCookie.serialize(),
-		},
-	});
 }
 
 export async function sendVerificationCode(email: string, code: string): Promise<void> {
